feb235658109f20de053b1974f79997bc6524553
kdinesh1234 <158142344+kdinesh1234@users.noreply.github.com>
2024-02-07 01:15:21 -0800

    Add files via upload

---
+++ (function oddnumber() {.js
@@ -0,0 +1,65 @@
+(function oddnumber() {
+    print: oddnumber in Array;
+
+    console.log[oddnumber];
+}  (1,3,5,7 ))
+
+_______________________________
+
+
+(function string() {
+    string = [ "apple", "orange", "mango"];
+    console.logstringword ("title caps"); 
+}  ( apple,orange,mango))
+
+
+_______________________________
+
+(function  sum (...nums){
+    total = 0
+    for (let index =0 ; index< nums.length ; index++);
+    total = total + nums[index];
+
+}  [1, 2 ,3 ,4 ,5, 6, 7, 8, 9])
+
+_______________________________
+
+
+function palindrome ( GOD, LEVEL, RADAR, VALUE,CIVIC){
+    return (palindrome);
+    console.log[palindrome];
+}
+
+_______________________________
+
+
+(function median (){
+     value: [ 12345678];
+    return median [4+5];
+    n = 9;
+    console.log (divide (n));
+
+} )
+
+________________________________
+
+
+ (oddnumber => {
+    print: oddnumber in Array;
+
+    console.log[oddnumber];
+} ) (1,3,5,7 )
+
+_______________________________
+
+ (string=> {
+    string = [ "apple", "orange", "mango"];
+    console.logstringword ("title caps"); 
+} ) ( apple,orange,mango)
+
+________________________________________________
+
+( palindrome =>{
+    return (palindrome);
+    console.log[palindrome];
+}) ( GOD, LEVEL, RADAR, VALUE,CIVIC)
